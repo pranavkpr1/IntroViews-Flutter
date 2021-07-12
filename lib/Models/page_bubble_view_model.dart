5be43d2420716c5d613d94ca2fe29cbbe8387b90
MohitJLalwani <71822230+MohitJLalwani@users.noreply.github.com>
2021-07-12 21:43:43 +0530

    Update page_bubble_view_model.dart

--- lib/Models/page_bubble_view_model.dart
+++ lib/Models/page_bubble_view_model.dart
@@ -11,9 +11,9 @@ class PageBubbleViewModel {
   final Widget? bubbleInner;
 
   PageBubbleViewModel({
-    this.bubbleInner!,
-   this.iconAssetPath!,
-     this.iconColor!,
+   required this.bubbleInner,
+  required this.iconAssetPath,
+    required this.iconColor,
     required this.isHollow,
     required this.activePercent,
     this.bubbleBackgroundColor = const Color(0x88FFFFFF),
